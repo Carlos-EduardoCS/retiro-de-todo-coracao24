084c5e6a730e177692046ad54c56be6596f770e4
Gusta 😎 <gstcarv@gmail.com>
2024-01-06 17:50:26 -0300

    fix(countdown): countdown not working as expected

--- js/countdown.js
+++ js/countdown.js
@@ -3,34 +3,29 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const segsEl = document.getElementById("segs");
 
-const newYears = "10 Fev 2024"
+const newYears = "2024-02-10T00:00:00";
 
-function countDown(){
-
+function countDown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate.getSeconds() - currentDate.getSeconds()) / 1000
-    
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60)  % 60;
+    const mins = Math.floor(totalSeconds / 60) % 60;
     const segs = Math.floor(totalSeconds) % 60;
-    console.log(totalSeconds);
-    
+
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML =  formatTime(mins);
-    segsEl.innerHTML =  formatTime(segs);
-
+    minsEl.innerHTML = formatTime(mins);
+    segsEl.innerHTML = formatTime(segs);
 }
 
 function formatTime(time) {
-    return time < 10 ? `0${time}` : time 
+    return time < 10 ? `0${time}` : time;
 }
 
 countDown();
 
 setInterval(countDown, 1000);
-

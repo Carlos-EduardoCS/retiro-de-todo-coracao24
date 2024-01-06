faa87dc671f360cdf9cdd1196c1ce002caabcaa9
Carlos Eduardo <128542237+Carlos-EduardoCS@users.noreply.github.com>
2024-01-06 09:58:55 -0300

    fixing the bug

--- js/script.js
+++ js/script.js
@@ -6,8 +6,96 @@ window.sr = ScrollReveal({
     
 });
 
+sr.reveal('.button2', {
+    delay: 200,
+    origin: 'left',
+    duration: 1000,
+});
+
 sr.reveal('button', {
     delay: 200,
     origin: 'left',
     duration: 1000,
 });
+
+sr.reveal('.logo', {
+    delay: 290,
+    origin: 'top',
+    duration: 1050,
+});
+
+
+sr.reveal('.date1', {
+    delay: 290,
+    origin: 'left',
+    duration: 700,
+});
+
+sr.reveal('.date2', {
+    delay: 350,
+    origin: 'left',
+    duration: 1000,
+});
+
+sr.reveal('.date3', {
+    delay: 410,
+    origin: 'left',
+    duration: 2000,
+});
+
+sr.reveal('.mes', {
+    delay: 410,
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.map', {
+    delay: 410,
+    origin: 'top',
+    duration: 2000,
+});
+
+sr.reveal('.mov1', {
+    delay: 410,
+    origin: 'left',
+    duration: 2000,
+});
+
+sr.reveal('.bx', {
+    delay: 410,
+    origin: 'top',
+    duration: 2000,
+});
+
+sr.reveal('.bx3', {
+    delay: 410,
+    origin: 'left',
+    duration: 2000,
+});
+sr.reveal('.bx2', {
+    delay: 410,
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.hearts', {
+    delay: 410,
+    origin: 'bottom',
+    duration: 2000,
+});
+
+sr.reveal('.person', {
+    delay: 200,
+    origin: 'top',
+    duration: 2000,
+});
+
+sr.reveal('.line-up-ttl', {
+    delay: 200,
+    origin: 'left',
+    duration: 2000,
+});
+
+
+
+
